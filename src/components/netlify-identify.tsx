36eb647bca7579c8358f4d3714b8203aa29e5298
Rachel Wallis <rachelwalliswork@gmail.com>
2024-08-15 20:37:32 +0100

    modify netlify identify

--- src/components/netlify-identify.tsx
+++ src/components/netlify-identify.tsx
@@ -1,9 +1,18 @@
-// ** NOTE: This page is components/netlify-identify.tsx **
+import { useEffect } from 'react';
 
 const NetlifyIdentity: React.FC = () => {
-  return (
-    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-  );
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "https://identity.netlify.com/v1/netlify-identity-widget.js";
+    script.async = true; // Load the script asynchronously
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script); // Clean up the script when the component is unmounted
+    };
+  }, []);
+
+  return null; // This component doesn't need to render anything
 };
 
 export default NetlifyIdentity;

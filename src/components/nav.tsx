1d0b08d14f8c060867e6314cb17db7bd8af8d822
Rachel Wallis <rachelwalliswork@gmail.com>
2024-08-15 19:11:22 +0100

    correct version of decap and admin page working

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -12,6 +12,20 @@ const MainNav: React.FC = () => {
             <small>Your web transformation partner</small>
           </Link>
         </li>
+        <div className="test-nav">
+          <p>Test Pages</p>
+          <ul>
+              <li>
+                <Link href="/blog">Blog</Link>
+              </li>
+              <li>
+                <Link href="/proposal">Proposal</Link>
+              </li>
+              <li>
+                <Link href="/admin">CMS Admin</Link>
+              </li>
+          </ul>
+        </div>
         <div>
           <li>
             <Link href="/">Book a consultation</Link>
